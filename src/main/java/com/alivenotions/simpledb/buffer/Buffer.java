53a04ef24545a6543d888f6c2fc9e8a40225e42a
Bhavdeep Dhanjal <bhavdeep@hey.com>
2024-11-24 03:46:16 +0100

    first try at buffer management

---
+++ src/main/java/com/alivenotions/simpledb/buffer/Buffer.java
@@ -0,0 +1,67 @@
+package com.alivenotions.simpledb.buffer;
+
+import com.alivenotions.simpledb.file.Block;
+import com.alivenotions.simpledb.file.FileManager;
+import com.alivenotions.simpledb.file.Page;
+import com.alivenotions.simpledb.log.LogManager;
+
+public class Buffer {
+  private final FileManager fileManager;
+  private final LogManager logManager;
+  private final Page page;
+  private Block block = null;
+  private int pins = 0;
+  private int txNum = -1;
+  private int lsn = -1;
+
+  public Buffer(FileManager fileManager, LogManager logManager) {
+    this.fileManager = fileManager;
+    this.logManager = logManager;
+    page = Page.newPage(fileManager.blockSize());
+  }
+
+  public Page page() {
+    return page;
+  }
+
+  public Block block() {
+    return block;
+  }
+
+  public void setModified(int txNum, int lsn) {
+    this.txNum = txNum;
+    // How is negative passed here and how does that help?
+    if (lsn >= 0) this.lsn = lsn;
+  }
+
+  public boolean isPinned() {
+    return pins > 0;
+  }
+
+  public int modifyingTx() {
+    return txNum;
+  }
+
+  void assignToBlock(Block block) {
+    flush();
+    this.block = block;
+    fileManager.readBlockIntoPage(this.block, page);
+    pins = 0;
+  }
+
+  void flush() {
+    if (txNum >= 0) {
+      logManager.flush(lsn);
+      fileManager.writePageToBlock(block, page);
+      txNum = -1;
+    }
+  }
+
+  void pin() {
+    pins++;
+  }
+
+  void unpin() {
+    pins--;
+  }
+}
---
+++ src/main/java/com/alivenotions/simpledb/buffer/BufferManager.java
@@ -0,0 +1,99 @@
+package com.alivenotions.simpledb.buffer;
+
+import com.alivenotions.simpledb.file.Block;
+import com.alivenotions.simpledb.file.FileManager;
+import com.alivenotions.simpledb.log.LogManager;
+
+public class BufferManager {
+  private Buffer[] bufferPool;
+  private int numAvailable;
+  private static final long MAX_TIME_IN_MS = 10 * 1000;
+
+  public BufferManager(FileManager fileManager, LogManager logManager, int numBuffs) {
+    this.bufferPool = new Buffer[numBuffs];
+    numAvailable = numBuffs;
+    for (int i = 0; i < numBuffs; i++) {
+      bufferPool[i] = new Buffer(fileManager, logManager);
+    }
+  }
+
+  public synchronized int available() {
+    return numAvailable;
+  }
+
+  public synchronized void flushAll(int txNum) {
+    for (Buffer buffer : bufferPool) {
+      if (buffer.modifyingTx() == txNum) {
+        buffer.flush();
+      }
+    }
+  }
+
+  public synchronized void unpin(Buffer buffer) {
+    buffer.unpin();
+    if (!buffer.isPinned()) {
+      numAvailable++;
+      // How is this working?
+      notifyAll();
+    }
+  }
+
+  public synchronized Buffer pin(Block block) {
+    try {
+      long timestamp = System.currentTimeMillis();
+      Buffer buffer = tryToPin(block);
+      while (buffer == null && !waitingTooLong(timestamp)) {
+        // how is this working?
+        wait(MAX_TIME_IN_MS);
+        buffer = tryToPin(block);
+      }
+      if (buffer == null) {
+        throw new BufferAbortException();
+      }
+      return buffer;
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private boolean waitingTooLong(long startTime) {
+    return System.currentTimeMillis() - startTime > MAX_TIME_IN_MS;
+  }
+
+  private Buffer tryToPin(Block block) {
+    Buffer buffer = findExistingBuffer(block);
+
+    if (buffer == null) {
+      buffer = chooseUnpinnedBuffer();
+      if (buffer == null) {
+        return null;
+      }
+      buffer.assignToBlock(block);
+    }
+
+    if (!buffer.isPinned()) {
+      numAvailable--;
+    }
+    buffer.pin();
+    return buffer;
+  }
+
+  private Buffer findExistingBuffer(Block block) {
+    for (Buffer buffer : bufferPool) {
+      Block possibleBufferBlock = buffer.block();
+      if (possibleBufferBlock != null && possibleBufferBlock.equals(block)) {
+        return buffer;
+      }
+    }
+    return null;
+  }
+
+  private Buffer chooseUnpinnedBuffer() {
+    for (Buffer buffer : bufferPool) {
+      if (!buffer.isPinned()) {
+        return buffer;
+      }
+    }
+    return null;
+  }
+}
