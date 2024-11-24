1a92391574abda55b72eb076d69a921e0e1626ea
Bhavdeep Dhanjal <bhavdeep@hey.com>
2024-11-24 03:08:39 +0100

    test file hygiene

--- src/test/java/com/alivenotions/simpledb/file/FileTest.java
+++ src/test/java/com/alivenotions/simpledb/file/FileTest.java
@@ -4,16 +4,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.alivenotions.simpledb.server.SimpleDB;
+import java.io.File;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class FileTest {
+  private FileManager fileManager;
+  private static final String TEST_FILE_NAME = "testfile";
+
+  @BeforeEach
+  void setup() {
+    SimpleDB db = new SimpleDB("testdb", 400, 8);
+    fileManager = db.fileManager();
+  }
+
+  @AfterEach
+  void tearDown() {
+    File dir = new File("testdb");
+    File[] files = dir.listFiles();
+    assert files != null;
+    for (File file : files) {
+      file.delete();
+    }
+  }
+
   @Test
   public void writing_to_the_db_works() {
-
-    SimpleDB db = new SimpleDB("testdb", 400, 8);
-    FileManager fileManager = db.fileManager();
-
-    Block block = new Block("testfile", 2);
+    Block block = new Block(TEST_FILE_NAME, 2);
 
     Page page1 = Page.newPage(fileManager.blockSize());
     int pos1 = 88;
@@ -32,10 +50,7 @@ public class FileTest {
 
   @Test
   public void file_manager_tracks_stats() {
-    SimpleDB db = new SimpleDB("testdb", 400, 8);
-    FileManager fileManager = db.fileManager();
-
-    Block block = new Block("testfile", 2);
+    Block block = new Block(TEST_FILE_NAME, 2);
 
     Page page1 = Page.newPage(fileManager.blockSize());
     int pos1 = 88;
@@ -51,17 +66,13 @@ public class FileTest {
     Page page3 = Page.newPage(fileManager.blockSize());
     fileManager.readBlockIntoPage(block, page3);
 
-    FileStatistics stats = fileManager.fileStatistics("testfile");
+    FileStatistics stats = fileManager.fileStatistics(TEST_FILE_NAME);
     assertEquals(1, stats.blocksWritten());
     assertEquals(2, stats.blocksRead());
   }
 
   @Test
   public void file_manager_throws_when_stats_file_does_not_exist() {
-
-    SimpleDB db = new SimpleDB("testdb", 400, 8);
-    FileManager fileManager = db.fileManager();
-
-    assertThrows(IllegalArgumentException.class, () -> fileManager.fileStatistics("testfile"));
+    assertThrows(IllegalArgumentException.class, () -> fileManager.fileStatistics(TEST_FILE_NAME));
   }
 }
